7627163618bd09629ae092e8a2625c1c186fd859
kujawskip <patryk.kujawski93@gmail.com>
2015-06-20 22:34:45 +0200

    fix

--- serverUDP.c
+++ serverUDP.c
@@ -270,6 +270,7 @@ ssize_t bulk_fread(FILE* fd,char* buf,size_t count)
 		
 		c=TEMP_FAILURE_RETRY(fread(buf,1,count,fd));
 		fprintf(stderr,"DEBUG: Fread %d\n",c);
+		if(c==0) break;
 		if(c<0) return c;
 		buf+=c;
 		len+=c;
