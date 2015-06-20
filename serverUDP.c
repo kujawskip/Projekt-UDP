9ea6e9818b511ae27b6c6e1239bc2094fe1537d7
kujawskip <patryk.kujawski93@gmail.com>
2015-06-20 22:33:01 +0200

    debug msg 2

--- serverUDP.c
+++ serverUDP.c
@@ -267,7 +267,9 @@ ssize_t bulk_fread(FILE* fd,char* buf,size_t count)
 	size_t len=0;
 	do
 	{
+		
 		c=TEMP_FAILURE_RETRY(fread(buf,1,count,fd));
+		fprintf(stderr,"DEBUG: Fread %d\n",c);
 		if(c<0) return c;
 		buf+=c;
 		len+=c;
