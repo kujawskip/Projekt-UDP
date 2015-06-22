919c26a0e6c08b0903497a9a076255293f1c5485
kujawskip <patryk.kujawski93@gmail.com>
2015-06-22 23:26:27 +0200

    fix 8

--- clientUDP.c
+++ clientUDP.c
@@ -658,7 +658,7 @@ void StartOperation(int sendfd,int listenfd,struct sockaddr_in address,char* dat
 {
 	trarg->sendfd = sendfd;
 	trarg->listenfd=listenfd;
-	memcpy((void*)&(trarg.address),(void*)&address,sizeof(struct sockaddr_in));
+	memcpy((void*)&(trarg->address),(void*)&address,sizeof(struct sockaddr_in));
 	trarg->restart=restart;
 	trarg.Kind=Kind;
 	strcpy(trarg->data,data);
